66493180adfb4aa1cb4ccd3ff9307ceae378c9b3
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-04 19:31:28 -0500

    Agrega la instancia a la clase ExplorerService y sus pruebas rápidas

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Reader = require('./lib/utils/Reader')
-const explorers = Reader.readJsonFile("./explorers.json")
+const explorers = Reader.readJsonFile("./explorers.json") // retorna la lista de explorers del archivo
+// console.log(explorers)
 
-console.log(explorers)
+const ExplorerService = require('./lib/services/ExplorerService')
+
+// console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
