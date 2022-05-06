33c08cf263da667ef8c908d9bb75e6cfa32a17c6
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-05 19:19:00 -0500

    Corrigiendo prueba

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,6 +4,6 @@ describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{ mission: "node" }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(666)
+        expect(explorersInNode.length).toBe(1)
     })
 })
