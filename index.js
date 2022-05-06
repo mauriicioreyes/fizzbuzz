152bc84d8c73626369b0cfa18211cd3ad7b036ce
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-05 21:08:56 -0500

    Ajustando archivos con ESLint

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const Reader = require('./lib/utils/Reader')
-const explorers = Reader.readJsonFile("./explorers.json") // retorna la lista de explorers del archivo
+const Reader = require("./lib/utils/Reader");
+const explorers = Reader.readJsonFile("./explorers.json"); // retorna la lista de explorers del archivo
 // console.log(explorers)
 
-const ExplorerService = require('./lib/services/ExplorerService')
-const FizzbuzzService = require('./lib/services/FizzbuzzService')
+const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
 // console.log(ExplorerService.filterByMission(explorers, "node"))
 // console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 // console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
 
-const explorer1 = { name: "Explorer1", score: 1 }
-FizzbuzzService.applyValidationInEXplorer(explorer1)
+const explorer1 = { name: "Explorer1", score: 1 };
+FizzbuzzService.applyValidationInEXplorer(explorer1);
 
-const explorer3 = { name: "Explorer3", score: 3 }
-FizzbuzzService.applyValidationInEXplorer(explorer3)
+const explorer3 = { name: "Explorer3", score: 3 };
+FizzbuzzService.applyValidationInEXplorer(explorer3);
 
-const explorer5 = { name: "Explorer5", score:5 }
-FizzbuzzService.applyValidationInEXplorer(explorer5)
+const explorer5 = { name: "Explorer5", score:5 };
+FizzbuzzService.applyValidationInEXplorer(explorer5);
 
-const explorer15 = { name: "Explorer15", score: 15 }
-FizzbuzzService.applyValidationInEXplorer(explorer15)
+const explorer15 = { name: "Explorer15", score: 15 };
+FizzbuzzService.applyValidationInEXplorer(explorer15);
 
-const explorer30 = { name: "Mauricio Reyes", score: 30 }
-FizzbuzzService.applyValidationInEXplorer(explorer30)
+const explorer30 = { name: "Mauricio Reyes", score: 30 };
+FizzbuzzService.applyValidationInEXplorer(explorer30);
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,9 +1,9 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{ mission: "node" }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1)
-    })
-})
+        expect(explorersInNode.length).toBe(1);
+    });
+});
